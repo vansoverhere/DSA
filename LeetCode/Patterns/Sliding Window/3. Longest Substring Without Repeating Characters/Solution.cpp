345c42fac38093686cad30d032b58b59ec1ac1d0
Vanshika Sharma <168810759+vansoverhere@users.noreply.github.com>
2026-09-04 16:58:25 +0530

    Add Medium C++ Solution for 3. Longest Substring Without Repeating Characters from LeetCode

--- LeetCode/Patterns/Sliding Window/3. Longest Substring Without Repeating Characters/Solution.cpp
+++ LeetCode/Patterns/Sliding Window/3. Longest Substring Without Repeating Characters/Solution.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map <char,int> m;
+        vector<int>m(256,-1);
         int l=0;
         int r=0;
         int len=0;
         int maxLen=0;
 
         while(r < s.size()){
-            if(m.find(s[r]) != m.end()){
+            if(m[s[r]]!=-1){
                 if(m[s[r]] >=l ){
                     l=m[s[r]]+1;
                 }
